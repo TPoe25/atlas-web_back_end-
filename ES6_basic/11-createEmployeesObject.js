1b5cc0b7b0b999b2f31d54594a0ab348490daa64
TPoe25 <t_poe25@yahoo.com>
2024-01-11 11:34:48 -0600

    11 lint fix

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
-export default function createEmployeesObject(departmentName, employees){
-    const departmentObject = {};
-    departmentObject[departmentName] = employees;
-    return departmentObject;
+export default function createEmployeesObject(departmentName, employees) {
+  const departmentObject = {};
+  departmentObject[departmentName] = employees;
+  return departmentObject;
 }
