2aaa8683e2d282901f6057a9d6cc9fa81315c58f
TPoe25 <t_poe25@yahoo.com>
2024-01-11 11:37:27 -0600

    12 fix

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeeList) {
-    return {
-      allEmployees: employeeList,
-      getNumberOfDepartments: function (allEmployees) {
-        return Object.keys(this.allEmployees).length;
-      },
+  return {
+    allEmployees: employeeList,
+    getNumberOfDepartments: function (allEmployees) {
+      return Object.keys(this.allEmployees).length;
+    },
   };
 }
