8b93a3706d659de3354ecdfeff0bfea32525b32e
TPoe25 <t_poe25@yahoo.com>
2025-01-19 20:33:12 -0600

    fixed doc

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,8 @@ import random
 
 
 async def wait_random(max_delay: int) -> float:
+    """ Waits for a random delay between 0 and max_delay seconds & returns it
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
