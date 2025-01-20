3b60213ecb1366f990cf63b271a7085b01a08e00
TPoe25 <t_poe25@yahoo.com>
2025-01-19 20:03:01 -0600

    check

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -12,6 +12,12 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and max_delay seconds
     and returns the delay.
+    
+    Args:
+        max_delay (int, optional): Maximum delay in seconds. Defaults to 10.
+
+    Returns:
+        float: The delay in seconds.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,34 +1,13 @@
-#!/usr/bin/env python3
-"""
-Demonstrates asynchronous programming using asyncio and random
-delays
-"""
-
 from typing import List
 import asyncio
-import importlib
-# Dynamically import wait_random
-wait_random = getattr(importlib.import_module("0-basic_async_syntax"),
-                      "wait_random")
+import random
 
+async def wait_random(max_delay: int) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 
-async def wait_n(n: int, max_delay: float) -> List[float]:
-    """
-    Coroutine spawns `n` wait_random coroutines w/ a max_delay n returns result
-
-    Args:
-        n (int): number of wait_random coroutines to run at same time
-        max_delay (int): Maximum delay for the random numbers.
-
-    Returns:
-        List[float]: List of delays in ascending order.
-    """
-    delays = []
-    for _ in range(n):
-        delays.append(asyncio.create_task(wait_random(max_delay)))
-
-    completed_delay = []
-    for task in asyncio.as_completed(delays):
-        completed_delay.append(await task)
-
-    return completed_delay
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
