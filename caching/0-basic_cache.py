411f8d5c8ce314eb738421b6683d3891544a2854
TPoe25 <t_poe25@yahoo.com>
2025-01-26 19:01:32 -0600

    changing file after crash

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,12 +4,6 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     def put(self, key, item):
-        item: self.cache_data
-        if key or item is None:
-            return None
-        if self.cache[key] is item:
-            return self.cache_data
-        
-    def get(self, key):
-        if key is self.cache_data:
-            print(self.cache_data[key])
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
